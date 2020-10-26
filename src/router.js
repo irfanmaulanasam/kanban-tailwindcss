6cfa1de7ada0778d05f4dc128c0f7345cc2fd754
irfan maulanasam <alternatiflab@gmail.com>
2020-10-26 19:49:58 +0700

    adding something new

--- src/router.js
+++ src/router.js
@@ -5,11 +5,14 @@ import Home from '@/views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',
   routes:[
     {
       path:'/',
       name:'home',
-      components: Home
-    }
-  ]
+      component: Home
+    },
+  ],
+  base:'/home',
+  root:'/'
 })
